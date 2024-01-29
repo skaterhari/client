93511a8510445a95c8d9aa2d1e681d46d2581bc4
skaterhari <harisundar03@gmail.com>
2024-01-29 23:26:41 +0530

    url modify

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -10,7 +10,8 @@ export const fetchUsers=createAsyncThunk('fetchUsers',async()=>{
    
     try {
       console.log(process.env.REACT_APP_SERVER_URI)
-       const url=`${process.env.REACT_SERVER_URI}/fetchUsers`;
+      const url=process.env.REACT_APP_SERVER_URI.concat('/fetchUsers')
+      
        console.log(url)
         const response = await axios.get(url);
         return response.data.user; // Assuming your server returns the user object
@@ -23,7 +24,7 @@ export const editUser=createAsyncThunk('editUser',async(credentials)=>{
    
   try {
       console.log(credentials.id)
-      const url=`${process.env.REACT_SERVER_URI}/editUser/${credentials.id}`;
+      const url=process.env.REACT_SERVER_URI.concat(`/editUser/${credentials.id}`);
       const response = await axios.put(url,credentials.formData);
       return response.data; // Assuming your server returns the user object
     } catch (error) {
@@ -32,7 +33,7 @@ export const editUser=createAsyncThunk('editUser',async(credentials)=>{
 })
 export const signUpUser=createAsyncThunk('signUpUser',async(credentials)=>{
   try{
-    const url=`${process.env.REACT_SERVER_URI}/api/signup`;
+    const url=process.env.REACT_SERVER_URI.concat('/api/signup');
         const res=await axios.post(url,credentials)
         return res.data;
   }
